4c3bdb3715db7e65b7d25cbd4cf188db2fd7d920
Steven R. Kitchens <srk0002@auburn.edu>
2020-03-16 19:59:52 -0500

    Fixe TestGetAllTranslations

--- translate.py
+++ translate.py
@@ -77,7 +77,19 @@ def get_all_translations(rna_sequence, genetic_code):
         A list of strings; each string is an sequence of amino acids encoded by
         `rna_sequence`.
     """
-    pass
+    rna_sequence = rna_sequence.upper()
+    bases = len(rna_sequence)
+    index = bases - 3
+    if len(rna_sequence) < 3:
+        return ''
+    amino_acid_seq_list = []
+    for base_index in range(bases +1):
+        codon = rna_sequence[base_index: base_index +3]
+        if codon == "AUG":
+            aa_seq = translate_sequence(rna_sequence = rna_sequence[base_index:], genetic_code = genetic_code)
+            if aa_seq:
+                amino_acid_seq_list.append(aa_seq)
+    return amino_acid_seq_list
 
 def get_reverse(sequence):
     """Reverse orientation of `sequence`.
