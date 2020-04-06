83df9b95fb53229220389be74ba458187105f7ec
Steven R. Kitchens <srk0002@auburn.edu>
2020-04-06 14:58:13 -0500

    Corrected TestVetNucleotideSequence, TestVetCodon, and TestFindFirstOrf. Needed an if, else statement for FindFirstOrf.

--- find_orf.py
+++ find_orf.py
@@ -57,8 +57,8 @@ def vet_nucleotide_sequence(sequence):
     # any valid RNA and DNA sequence strings, respectively (and only strings of
     # RNA and DNA bases).
     # Read the docstring above for additional clues.
-    rna_pattern_str = r'[AUCG]*$'
-    dna_pattern_str = r'[ATCG]*$'
+    rna_pattern_str = r'(?!.*T)[AUCG]*'
+    dna_pattern_str = r'(?!.*U)[ATCG]*'
     ##########################################################################
 
     rna_pattern = re.compile(rna_pattern_str)
@@ -119,7 +119,7 @@ def vet_codon(codon):
     # Change `codon_pattern_str` so that it will match any valid codons, and
     # only valid codons.
     # Read the docstring above for additional clues.
-    codon_pattern_str = r'[AUCGaucg]{3}$'
+    codon_pattern_str = r'(?!.Tt)^[AUGC]{3}$|^[augc]{3}$'
     ##########################################################################
 
     codon_pattern = re.compile(codon_pattern_str)
@@ -207,7 +207,10 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'AUGGUAUAA'
+    if start_codons == ['AUG']:
+        orf_pattern_str = r'AUG([AUCG]{3})*(UAG|UGA|UAA)'
+    else:
+        orf_pattern_str = r'AAA([AUCG]{3})*UUU'
     ##########################################################################
 
     # Create the regular expression object
